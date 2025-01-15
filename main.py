f3d878ea2b8aec3b6809b8153262a358413c7a67
Owen Wu <79323567+weewoo14@users.noreply.github.com>
2025-01-14 20:37:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,208 @@
+from tkinter import *
+from math import *
+from time import *
+from random import *
+
+root = Tk()
+screen = Canvas(root, width=1000, height=600, background='white')
+
+def createCoordinatePos():
+    global mazeCoordX, mazeCoordY, numMazeRow, numMazeCol
+    mazeCoordX = []
+    mazeCoordY = []
+    curSquareY = 50
+    for row in range(numMazeRow):
+        curSquareX = 50
+        coordRowX = []
+        coordRowY = []
+        for col in range(numMazeCol):
+            coordRowX.append(curSquareX)
+            coordRowY.append(curSquareY)
+            curSquareX = curSquareX + 100
+        curSquareY = curSquareY + 100
+        mazeCoordX.append(coordRowX)
+        mazeCoordY.append(coordRowY)
+
+def setInitialValues():
+    global playerX, playerY, playerXSpeed, playerYSpeed, playerConstSpeed, playerSize, playerSight, playerSightDist, playerMoveCooldown
+    global mazeArray, numMazeRow, numMazeCol, mazeSquareSpace, mazeSquares
+    global centreX, centreY
+
+    playerX = 750
+    playerY = 750
+    playerXSpeed = 0
+    playerYSpeed = 0
+    playerConstSpeed = 17
+    playerSight = 15
+    playerSightDist = (playerSight//2) * 100
+    playerSize = (600//playerSight) // 2
+    playerMoveCooldown = 0
+
+    mazeArray = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                ]
+
+    
+    numMazeRow = 39
+    numMazeCol = 39
+    mazeSquareSpace = 600 // playerSight
+    mazeSquares = []
+
+    centreX = 300
+    centreY = 300
+
+    createCoordinatePos()
+
+def drawMaze():
+    global playerSightDist
+    global mazeArray, numMazeRow, numMazeCol, mazeSquareSpace, mazeSquares, mazeCoordX, mazeCoordY
+
+    mazeSquareColours = [['black' for j in range(numMazeCol)] for i in range(numMazeRow)]
+    closestXPos = float('inf')
+    closestYPos = float('inf')
+    closestCol = 0
+    closestRow = 0
+
+    mazeSquares = []
+    for row in range(numMazeRow):
+        for col in range(numMazeCol):
+            if mazeArray[row][col] == 1:
+                mazeSquareColour = '#083806'
+            
+            else:
+                mazeSquareColour = '#382206'
+
+            mazeSquareX = mazeCoordX[row][col]
+            mazeSquareY = mazeCoordY[row][col]
+
+            if abs(playerX - mazeSquareX) < closestXPos:
+                closestXPos = abs(playerX - mazeSquareX)
+                closestCol = col
+            
+            if abs(playerY - mazeSquareY) < closestYPos:
+                closestYPos = abs(playerY - mazeSquareY)
+                closestRow = row
+
+            playerMazeSquareDist = sqrt((mazeSquareX - playerX)**2 + (mazeSquareY - playerY)**2)
+            if playerMazeSquareDist <= playerSightDist:
+                mazeSquareColours[row][col] = mazeSquareColour
+
+    placeSquareY = 0
+    for row in range(closestRow - playerSight//2, closestRow + playerSight//2 + 1):
+        placeSquareX = 0
+        for col in range(closestCol - playerSight//2, closestCol + playerSight//2 + 1):
+            mazeSquare = screen.create_rectangle(placeSquareX, placeSquareY, placeSquareX + mazeSquareSpace, placeSquareY + mazeSquareSpace, fill = mazeSquareColours[row][col], outline = mazeSquareColours[row][col])
+            mazeSquares.append(mazeSquare)
+            placeSquareX = placeSquareX + mazeSquareSpace
+        placeSquareY = placeSquareY + mazeSquareSpace
+
+def drawPlayer():
+    global playerX, playerY, playerSize
+    player = screen.create_rectangle(centreX - playerSize, centreY - playerSize, centreX + playerSize, centreY + playerSize, fill = 'red')
+
+def updatePlayerPosition():
+    global playerX, playerY, playerXSpeed, playerYSpeed
+    playerX = playerX + playerXSpeed
+    playerY = playerY + playerYSpeed
+    
+
+def updateObjects():
+    pass
+
+### KEYBIND SECTION ###
+def keyDownHandler(event):
+    global playerXSpeed, playerYSpeed, playerConstSpeed, playerMoveCooldown
+    global mazeArray, numMazeRow, numMazeCol
+
+    if playerMoveCooldown >= 0:
+        if event.keysym == "Left":
+            playerXSpeed = -playerConstSpeed
+        
+        if event.keysym == "Right":
+            playerXSpeed = playerConstSpeed
+        
+        if event.keysym == "Up":
+            playerYSpeed = -playerConstSpeed
+        playerMoveCooldown = 0
+        
+        if event.keysym == "Down":
+            playerYSpeed = playerConstSpeed
+
+def keyUpHandler(event):
+    global playerXSpeed, playerYSpeed
+    playerXSpeed = 0
+    playerYSpeed = 0
+
+
+def mouseClickHandler(event):
+    pass
+
+
+### END OF KEYBIND SECTION ###
+
+def runGame():
+    global playerMoveCooldown
+    global mazeSquares
+    screen.create_rectangle(785,0,800,15,fill='red')
+    setInitialValues()
+
+    while True:
+        drawMaze()
+        drawPlayer()
+
+        playerMoveCooldown += 1
+        screen.update()
+        sleep(0.03)
+        
+        for i in range(len(mazeSquares)):
+            screen.delete(mazeSquares[i])
+        
+        updatePlayerPosition()
+
+
+root.after(0,runGame)
+
+screen.bind("<Button-1>", mouseClickHandler)
+screen.bind("<Key>", keyDownHandler)
+screen.bind("<KeyRelease>", keyUpHandler)
+
+screen.pack()
+screen.focus_set()
+screen.mainloop()
